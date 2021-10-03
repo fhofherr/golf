425b08371d3fff8b311ea15379ef84b074af5f6f
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-10-03 21:52:08 +0200

    Add a doc.go file

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package golf provides the Go Logging Facade.
+//
+// The core of golf is the Logger interface. Various functions are defined
+// over this interface which make it easy to use, regardless of the underlying
+// logger implementation.
+package golf
